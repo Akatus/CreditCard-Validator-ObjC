d9b619b2d1ce10e1cff7deb67c0081f4bc3681f8
= <=>
2013-01-08 11:31:46 -0200

    credit card validator main class

---
+++ CreditCard-Validator-ObjC/CreditCard-Validator/CreditCard-Validator.h
@@ -0,0 +1,13 @@
+//
+//  CreditCard-Validator.h
+//  CreditCard-Validator-ObjC
+//
+//  Created by Fernando Bass on 1/8/13.
+//  Copyright (c) 2013 Fernando Bass. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CreditCard_Validator : NSObject
+
+@end
---
+++ CreditCard-Validator-ObjC/CreditCard-Validator/CreditCard-Validator.m
@@ -0,0 +1,13 @@
+//
+//  CreditCard-Validator.m
+//  CreditCard-Validator-ObjC
+//
+//  Created by Fernando Bass on 1/8/13.
+//  Copyright (c) 2013 Fernando Bass. All rights reserved.
+//
+
+#import "CreditCard-Validator.h"
+
+@implementation CreditCard_Validator
+
+@end
